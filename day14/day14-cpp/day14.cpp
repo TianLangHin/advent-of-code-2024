515915e4878145ab1de8940a3cac1140b21d4d40
TianLangHin <tianlanghin@gmail.com>
2024-12-14 22:20:43 +1100

    Day 14 C++ implementation (Part 2 added)

--- day14/day14-cpp/day14.cpp
+++ day14/day14-cpp/day14.cpp
@@ -43,6 +43,58 @@ void part1(robots_specs& robots) {
     std::cout << answer << "\n";
 }
 
+void part2(robots_specs& robots) {
+    const int width = 101;
+    const int height = 103;
+
+    for (int seconds = 1; seconds < width * height; ++seconds) {
+        bool grid[101][103] = {false};
+
+        for (int i = 0; i < robots.size(); ++i) {
+            int px = robots[i].first.first;
+            int py = robots[i].first.second;
+            int vx = robots[i].second.first;
+            int vy = robots[i].second.second;
+
+            int result_x = (px + vx) % width;
+            int result_y = (py + vy) % height;
+
+            if (result_x < 0)
+                result_x += width;
+            if (result_y < 0)
+                result_y += height;
+
+            robots[i] = {
+                { result_x, result_y },
+                { vx, vy }
+            };
+
+            grid[result_x][result_y] = true;
+        }
+
+        for (int i = 0; i < height; ++i) {
+            bool exit = false;
+            int run = 0;
+            for (int j = 0; j < width; ++j) {
+                if (grid[j][i]) {
+                    ++run;
+                } else {
+                    run = 0;
+                }
+                // This value of `width / 4` is estimated based on trial-and-error.
+                if (run > width / 4) {
+                    std::cout << seconds << "\n";
+                    exit = true;
+                    break;
+                }
+            }
+            if (exit) {
+                break;
+            }
+        }
+    }
+}
+
 int main() {
     std::fstream puzzle_input("day-14-puzzle-input.txt");
     std::string line;
@@ -72,4 +124,5 @@ int main() {
         }
     }
     part1(robots);
+    part2(robots);
 }
