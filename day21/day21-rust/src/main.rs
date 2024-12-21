41ccfd3a3385be95cd558e5788dcb04ddfe5bbdf
TianLangHin <tianlanghin@gmail.com>
2024-12-21 17:55:48 +1100

    Day 21 Rust implementation (Part 1 reduced code duplication)

--- day21/day21-rust/src/main.rs
+++ day21/day21-rust/src/main.rs
@@ -68,88 +68,56 @@ fn part1(codes: &Vec<&str>) {
         let min_length = first_robot.iter().map(|(s, _)| s.len()).min().unwrap();
         first_robot.retain(|(s, _)| s.len() == min_length);
 
-        let mut second_robot = Vec::new();
-        for (code, _) in first_robot {
-            let mut possible_solutions = VecDeque::from([("".to_string(), 0)]);
-            let steps = once('A').chain(code.chars()).zip(code.chars()).enumerate();
-            for (i, (start, end)) in steps {
-                let (x_start, y_start) = *dir_keypad.get(&start).unwrap();
-                let (x_end, y_end) = *dir_keypad.get(&end).unwrap();
-                let x_dist = (x_end - x_start).abs();
-                let y_dist = (y_end - y_start).abs();
+        let mut robot: Vec<_> = first_robot.into_iter().collect(); 
 
-                let x_key = if x_start < x_end { '>' } else { '<' };
-                let y_key = if y_start < y_end { 'v' } else { '^' };
-                let x_path = (0..x_dist).map(|_| x_key).collect::<String>();
-                let y_path = (0..y_dist).map(|_| y_key).collect::<String>();
+        for _ in 0..2 {
+            let mut next_robot = Vec::new();
+            for (code, _) in &robot {
+                let mut possible_solutions = VecDeque::from([("".to_string(), 0)]);
+                let steps = once('A').chain(code.chars()).zip(code.chars()).enumerate();
+                for (i, (start, end)) in steps {
+                    let (x_start, y_start) = *dir_keypad.get(&start).unwrap();
+                    let (x_end, y_end) = *dir_keypad.get(&end).unwrap();
+                    let x_dist = (x_end - x_start).abs();
+                    let y_dist = (y_end - y_start).abs();
 
-                let mut paths = BTreeSet::new();
-                if base_keypad.values().any(|&p| p == (x_end, y_start)) {
-                    paths.insert(x_path.clone() + &y_path);
-                }
-                if base_keypad.values().any(|&p| p == (x_start, y_end)) {
-                    paths.insert(y_path.clone() + &x_path);
-                }
+                    let x_key = if x_start < x_end { '>' } else { '<' };
+                    let y_key = if y_start < y_end { 'v' } else { '^' };
+                    let x_path = (0..x_dist).map(|_| x_key).collect::<String>();
+                    let y_path = (0..y_dist).map(|_| y_key).collect::<String>();
 
-                while !possible_solutions.is_empty() {
-                    let sol = possible_solutions.front().unwrap();
-                    if sol.1 > i {
-                        break;
+                    let mut paths = BTreeSet::new();
+                    if base_keypad.values().any(|&p| p == (x_end, y_start)) {
+                        paths.insert(x_path.clone() + &y_path);
                     }
-                    let item = possible_solutions.pop_front().unwrap().0;
-                    for path in &paths {
-                        possible_solutions.push_back(
-                            (format!("{}{}A", item, path), i + 1)
-                        );
+                    if base_keypad.values().any(|&p| p == (x_start, y_end)) {
+                        paths.insert(y_path.clone() + &x_path);
+                    }
+
+                    while !possible_solutions.is_empty() {
+                        let sol = possible_solutions.front().unwrap();
+                        if sol.1 > i {
+                            break;
+                        }
+                        let item = possible_solutions.pop_front().unwrap().0;
+                        for path in &paths {
+                            possible_solutions.push_back(
+                                (format!("{}{}A", item, path), i + 1)
+                            );
+                        }
                     }
                 }
+                next_robot.extend(possible_solutions.into_iter());
             }
-            second_robot.extend(possible_solutions.into_iter());
+
+            let min_length = next_robot.iter().map(|(s, _)| s.len()).min().unwrap();
+            next_robot.retain(|(s, _)| s.len() == min_length);
+
+            robot.clear();
+            robot.append(&mut next_robot);
         }
 
-        let min_length = second_robot.iter().map(|(s, _)| s.len()).min().unwrap();
-        second_robot.retain(|(s, _)| s.len() == min_length);
-
-        let mut third_robot = Vec::new();
-        for (code, _) in second_robot {
-            let mut possible_solutions = VecDeque::from([("".to_string(), 0)]);
-            let steps = once('A').chain(code.chars()).zip(code.chars()).enumerate();
-            for (i, (start, end)) in steps {
-                let (x_start, y_start) = *dir_keypad.get(&start).unwrap();
-                let (x_end, y_end) = *dir_keypad.get(&end).unwrap();
-                let x_dist = (x_end - x_start).abs();
-                let y_dist = (y_end - y_start).abs();
-
-                let x_key = if x_start < x_end { '>' } else { '<' };
-                let y_key = if y_start < y_end { 'v' } else { '^' };
-                let x_path = (0..x_dist).map(|_| x_key).collect::<String>();
-                let y_path = (0..y_dist).map(|_| y_key).collect::<String>();
-
-                let mut paths = BTreeSet::new();
-                if base_keypad.values().any(|&p| p == (x_end, y_start)) {
-                    paths.insert(x_path.clone() + &y_path);
-                }
-                if base_keypad.values().any(|&p| p == (x_start, y_end)) {
-                    paths.insert(y_path.clone() + &x_path);
-                }
-
-                while !possible_solutions.is_empty() {
-                    let sol = possible_solutions.front().unwrap();
-                    if sol.1 > i {
-                        break;
-                    }
-                    let item = possible_solutions.pop_front().unwrap().0;
-                    for path in &paths {
-                        possible_solutions.push_back(
-                            (format!("{}{}A", item, path), i + 1)
-                        );
-                    }
-                }
-            }
-            third_robot.extend(possible_solutions.into_iter());
-        }
-
-        let min_length = third_robot.iter().map(|(s, _)| s.len()).min().unwrap();
+        let min_length = robot.iter().map(|(s, _)| s.len()).min().unwrap();
         let numeric_part: usize = code.chars().take(3).collect::<String>().parse().unwrap();
 
         answer += min_length * numeric_part;
