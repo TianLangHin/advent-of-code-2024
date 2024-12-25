2552aef0c3ae5c18521613f51e7f05f733a9832a
TianLangHin <tianlanghin@gmail.com>
2024-12-25 16:25:18 +1100

    Day 25 Rust implementation (Part 1 reformat)

--- day25/day25-rust/src/main.rs
+++ day25/day25-rust/src/main.rs
@@ -1,18 +1,17 @@
 fn part1(schematics: &Vec<(Vec<i32>, bool)>) {
-    let mut answer = 0i64;
-    for (lock, is_lock) in schematics {
-        if !is_lock {
-            continue;
-        }
-        for (key, is_not_key) in schematics {
-            if *is_not_key {
-                continue;
-            }
-            if lock.iter().zip(key.iter()).all(|(&x, &y)| x + y <= 5) {
-                answer += 1;
-            }
-        }
-    }
+    let answer = schematics
+        .iter()
+        .map(|(lock, is_lock)| {
+            schematics
+                .iter()
+                .filter(|(key, is_not_key)| {
+                    !!is_lock
+                        && !is_not_key
+                        && lock.iter().zip(key.iter()).all(|(&x, &y)| x + y <= 5)
+                })
+                .count()
+        })
+        .sum::<usize>();
     println!("{answer}");
 }
 
@@ -26,7 +25,13 @@ fn main() {
             let length = rows[0].len();
             let is_lock = rows[0].chars().all(|s| s == '#');
             let heights = (0..length)
-                .map(|n| (rows.iter().filter(|r| r.chars().nth(n) == Some('#')).count() as i32) - 1)
+                .map(|n| {
+                    (rows
+                        .iter()
+                        .filter(|r| r.chars().nth(n) == Some('#'))
+                        .count() as i32)
+                        - 1
+                })
                 .collect();
             (heights, is_lock)
         })
